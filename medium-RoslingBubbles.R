6b36681de646ecc58280842d72b593bff81b17f0
Yufan Fei <62975717+Faye-yufan@users.noreply.github.com>
2023-04-04 10:37:11 -0700

    Improve Rosling Bubble

--- medium-RoslingBubbles.R
+++ medium-RoslingBubbles.R
@@ -1,36 +1,65 @@
 library(animint2)
+library(dplyr)
 
-Rosling.bubbles <- function(
-    x, y, data, nmax=50
-  ) {
-  n <- 10
-  if (missing(data)) data <- data.frame(year = rep(1:nmax, each = n))
-  n <- nrow(data)%/%nmax
-  if (missing(x)) x <- runif(n) + sort(rnorm(n * nmax, 0, 0.02))
-  if (missing(y)) y <- runif(n) + sort(rnorm(n * nmax, 0, 0.02))
-  data$x <- x
-  data$y <- y
-  data$bg <- rgb(runif(n), runif(n), runif(n), 0.5)
-  data$group <- 1:n
-  data$size <- runif(n * nmax, min = 2, max = 70)
-  
-  bubble <- ggplot(data) +
-    geom_point(aes(x = x, y = y, fill = bg, size = size, key = bg),
-               showSelected="year",
-               shape = 21, alpha = 0.5) + 
-    scale_fill_identity() +
-    scale_size(range = c(1, 70)) +
-    theme(legend.position = "none") +
-    xlab("x") +
-    ylab("y")
-  select.year <- ggplot()+
-    geom_tallrect(aes(
-      xmin=year-1/2, xmax=year+1/2),
-      clickSelects="year",
-      alpha=1/2,
-      data=data)
-  (viz <- animint(bubble, select.year,
-                 time=list(variable="year", ms=200)))
-}
+world.bank <-
+  # pull the country data down from the World Bank - three indicators
+  wbstats::wb_data(
+    indicator = c("SP.DYN.LE00.IN", "NY.GDP.PCAP.CD", "SP.POP.TOTL"), 
+    country = "countries_only", 
+    start_date = 1960, 
+    end_date = 2022
+  ) |> 
+  # pull down mapping of countries to regions and join   
+  dplyr::left_join(
+    wbstats::wb_countries()  |>  
+      dplyr::select(iso3c, region)
+  ) 
+world.bank$date <- as.numeric(world.bank$date)
 
-Rosling.bubbles()
+bubble <- ggplot(world.bank) +
+  geom_text(
+    aes(x = 7.5, y = 5, label = date, key = date), 
+    showSelected = "date",
+    size = 100, 
+    alpha = 0.2,
+    color = 'black', 
+    family = 'Oswald'
+  ) + 
+  geom_point(aes(x = log(NY.GDP.PCAP.CD), 
+                 y = SP.DYN.LE00.IN, 
+                 size = SP.POP.TOTL,
+                 fill = region, 
+                 key = country),
+             clickSelects = "country",
+             showSelected = "date",
+             colour = "black",
+             colour_off = "transparent",
+             stroke = 2,
+             alpha = 0.8,
+             alpha_off = 0.3,
+             shape = 21) + 
+  scale_fill_brewer(palette = "Set2") +
+  scale_size(range = c(1, 70)) +
+  theme(legend.position = "none") +
+  xlab("Log GDP per capita") +
+  ylab("Life expectancy") 
+
+select.year <- ggplot(data = world.bank) +
+  geom_tallrect(aes(
+    xmin = date - 1 / 2, xmax = date + 1 / 2),
+    clickSelects = "date",
+    alpha = 0.1)+
+  geom_line(aes(
+    date, SP.DYN.LE00.IN, group=country, colour=region),
+    clickSelects="country",
+    size=4, alpha=3/5) +
+  xlab("Year") +
+  ylab("Life expectancy")
+
+viz <- animint(bubble, select.year,
+               time = list(variable = "date", ms = 2000),
+               duration = list(date = 500),
+               first=list(date=1960, country=c("United States", "China")),
+               selector.types = list(country = "multiple"))
+
+viz
